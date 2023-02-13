2b9f7ad6a038e61cd35ce0f12990c0191d880de8
galcyurio <galcyurio@gmail.com>
2023-02-14 01:24:52 +0900

    feat: MVVM으로 마이그레이션한다

---
+++ calculator-app/src/main/java/camp/nextstep/edu/calculator/CalculatorViewModel.kt
@@ -0,0 +1,46 @@
+package camp.nextstep.edu.calculator
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import camp.nextstep.edu.calculator.domain.Calculator
+import camp.nextstep.edu.calculator.domain.Expression
+import camp.nextstep.edu.calculator.domain.Operator
+
+class CalculatorViewModel : ViewModel() {
+    private val calculator = Calculator()
+    private var expression = Expression.EMPTY
+
+    private val _text = MutableLiveData("")
+    val text: LiveData<String>
+        get() = _text
+
+    // TODO: SingleLiveEvent
+    private val _onCalculationErrorEvent = MutableLiveData<Unit>()
+    val onCalculationErrorEvent: LiveData<Unit>
+        get() = _onCalculationErrorEvent
+
+    fun addToExpression(operand: Int) {
+        expression += operand
+        _text.value = expression.toString()
+    }
+
+    fun addToExpression(operator: Operator) {
+        expression += operator
+        _text.value = expression.toString()
+    }
+
+    fun removeLast() {
+        expression = expression.removeLast()
+        _text.value = expression.toString()
+    }
+
+    fun calculate() {
+        val result = calculator.calculate(expression.toString())
+        if (result != null) {
+            _text.value = result.toString()
+        } else {
+            _onCalculationErrorEvent.value = Unit
+        }
+    }
+}
---
+++ calculator-app/src/test/java/camp/nextstep/edu/calculator/CalculatorViewModelTest.kt
@@ -0,0 +1,80 @@
+package camp.nextstep.edu.calculator
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import camp.nextstep.edu.calculator.domain.Operator
+import camp.nextstep.edu.calculator.util.getOrAwaitValue
+import com.google.common.truth.Truth.assertThat
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class CalculatorViewModelTest {
+    private lateinit var viewModel: CalculatorViewModel
+
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+    @Before
+    fun setUp() {
+        viewModel = CalculatorViewModel()
+    }
+
+    @Test
+    fun `피연산자를 수식에 추가`() {
+        // when
+        viewModel.addToExpression(1)
+
+        // then
+        val actual = viewModel.text.getOrAwaitValue()
+        assertThat(actual).isEqualTo("1")
+    }
+
+    @Test
+    fun `연산자를 수식에 추가`() {
+        // when
+        viewModel.addToExpression(1)
+        viewModel.addToExpression(Operator.Plus)
+
+        // then
+        val actual = viewModel.text.getOrAwaitValue()
+        assertThat(actual).isEqualTo("1 +")
+    }
+
+    @Test
+    fun `'1 + 2' 수식에서 마지막을 제거하면 '1 +'`() {
+        // when
+        viewModel.addToExpression(1)
+        viewModel.addToExpression(Operator.Plus)
+        viewModel.addToExpression(2)
+        viewModel.removeLast()
+
+        // then
+        val actual = viewModel.text.getOrAwaitValue()
+        assertThat(actual).isEqualTo("1 +")
+    }
+
+    @Test
+    fun `'1 + 2' 수식을 계산하면 3`() {
+        // when
+        viewModel.addToExpression(1)
+        viewModel.addToExpression(Operator.Plus)
+        viewModel.addToExpression(2)
+        viewModel.calculate()
+
+        // then
+        val actual = viewModel.text.getOrAwaitValue()
+        assertThat(actual).isEqualTo("3")
+    }
+
+    @Test
+    fun `'1 +' 수식을 계산하면 에러`() {
+        // when
+        viewModel.addToExpression(1)
+        viewModel.addToExpression(Operator.Plus)
+        viewModel.calculate()
+
+        // then
+        val actual = viewModel.onCalculationErrorEvent.getOrAwaitValue()
+        assertThat(actual).isNotNull()
+    }
+}
